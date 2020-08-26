e624330c2b65b294a8aa9cad32f09eda1d5d8903
Jordan <jordan.a.neill@gmail.com>
2020-08-25 23:25:49 -0500

    fixed routes

--- models/workout.js
+++ models/workout.js
@@ -4,12 +4,11 @@ const Schema = mongoose.Schema;
 
 //schema: exercises fields
 const ExerciseSchema = new Schema({
-  type: { type: String, trim: true, required: true },
-  name: { type: String, trim: true, required: true },
-  duration: { type: Number, trim: true, required: true },
-  weight: { type: Number, trim: true, required: true },
-  reps: { type: Number, trim: true, required: true },
-  sets: { type: Number, trim: true, required: true },
+  day: {
+    type: Number,
+    default: new Date().setDate(new Date().getDate()),
+  },
+  exercises: Array,
 });
 
 const Exercise = mongoose.model("Exercise", ExerciseSchema);
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const db = require("./models");
 
 const app = express();
 
+const path = require("path");
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -16,17 +18,17 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // html routes.
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./public/index.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-// app.get("/exercise", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./public/exercise.html"));
-// });
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
 
-// app.get("/stats", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./public/stats.html"));
-// });
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
 
 // TODO: modify code below for api routes.
 //connects to workout db/ deployment process.
@@ -42,58 +44,12 @@ db.Workout.create({ name: "Workout Plan!" })
     console.log(message);
   });
 
-app.get("/notes", (req, res) => {
-  db.Note.find({})
-    .then((dbNote) => {
-      res.json(dbNote);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.get("/user", (req, res) => {
-  db.User.find({})
-    .then((dbUser) => {
-      res.json(dbUser);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.post("/submit", ({ body }, res) => {
-  db.Note.create(body)
-    .then(({ _id }) =>
-      db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true })
-    )
-    .then((dbUser) => {
-      res.json(dbUser);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.get("/populateduser", (req, res) => {
-  db.User.find({})
-    .populate("notes")
-    .then((dbUser) => {
-      res.json(dbUser);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
 app.get("/api/workouts", (req, res) => {
-  db.Workout.find({})
-    .then((response) => {
-      res.json(response);
-    })
-    .catch((err) => {
-      res.json(err.message);
-    });
+  db.Workout.find({}).then((response) => {
+    console.log(response);
+    res.json(response);
+  });
+  //
 });
 
 app.put("/api/workouts/:id", async (req, res) => {
